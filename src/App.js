7497caea31bf91d862339761edc904c50be1680f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2023-02-16 16:02:21 +0530

    :sparkles: Routes ( basic, new place,  default navigate), NewPlace Page, Users and UsersList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import { Route, Routes, Navigate } from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./user/pages/Users";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Users />} />
+        <Route exact path="/places/new" element={<NewPlace />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
