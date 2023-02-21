68877e8511af0e436e406eb014695d33171c9d50
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2023-02-21 12:45:20 +0530

    :lipstick: Main Header and Main Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
 import Users from "./user/pages/Users";
 
 const App = () => {
+  // Check here if any UI alignment issue
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<Users />} />
-        <Route exact path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Users />} />
+          <Route exact path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </>
   );
 };
