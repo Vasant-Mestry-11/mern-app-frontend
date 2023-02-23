7026116a7cd9923dbc8848b6ffad22b661ff99ea
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2023-02-23 13:19:31 +0530

    :sparkles: :lipstick: Button Component

--- src/places/components/PlaceItem/PlaceItem.js
+++ src/places/components/PlaceItem/PlaceItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../../../shared/components/FormElements/Button/Button";
 import Card from "../../../shared/components/UIElements/Card/Card";
 import "./PlaceItem.css";
 
@@ -23,9 +24,9 @@ const PlaceItem = ({
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
